19ed0df6db665cdb84c2291e1e92f2cfe64609f8
slayer_0 <87zhaoxiaobo@163.com>
2022-08-04 18:09:15 +0800

    后期效果

--- src/shaders/frameBuffer/fragment.fs
+++ src/shaders/frameBuffer/fragment.fs
@@ -5,6 +5,10 @@ in vec2 v_texcoord;
 out vec4 finalColor;
 
 void main(){
-    finalColor = texture(screenTexture, v_texcoord);
-    // finalColor = vec4(v_texcoord, 0.0, 1.0);
+    vec3 c = texture(screenTexture, v_texcoord).rgb;
+    float averge = (0.2126 * c.r + 0.7152 * c.g + 0.0722 * c.b) / 3.0;
+    //finalColor = vec4(vec3(averge), 1.0); 灰度
+    //finalColor = vec4(1.0 - c.r, 1.0 - c.g, 1.0 - c.b, 1.0);  反相
+    //finalColor = vec4(v_texcoord, 0.0, 1.0); //正常
+    //
 }
